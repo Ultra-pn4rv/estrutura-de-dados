2c70c867aec390e63ce920641b86ade881fbf919
Ultra-pn4rv <tsarultra@gmail.com>
2024-05-13 09:53:28 -0400

    método Object-entries

---
+++ objetos/Object-entries.js
@@ -0,0 +1,8 @@
+const person = {
+    name: "Wanderson",
+    age: 40,
+    city: "Cuiabá",
+};
+const entries = Object.entries(person);
+console.log(entries);
+// Saída: [['name, 'Wanderson], ['age', 40], ['city', 'Cuiabá']]
